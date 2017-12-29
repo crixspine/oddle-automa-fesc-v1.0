d67cb18553a7b2ea45befe3d5646030236c9cb98
Ang <Ang@DESKTOP-JMN7MR3>
2017-12-29 19:59:11 +0800

    29/12/2017(2)

---
+++ Object Repository/Account/span_Ensure it contains at lea.rs
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_Ensure it contains at lea</name>
+   <tag></tag>
+   <elementGuidId>37836f25-5539-4654-9815-de60875fbb55</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>dot6ul-4 kFSzbP</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Ensure it contains at least 8 characters</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;body&quot;)/div[@class=&quot;s134bqp5-1 cNgNgb&quot;]/div[@class=&quot;s1ikpaay-4 dAQshL&quot;]/div[@class=&quot;m7p8f0-0 fUTidt&quot;]/div[@class=&quot;e9phw6-1-sc-bdVaJa iriZUy e9phw6-0 jBJmyp&quot;]/div[@class=&quot;cmag0j-1-sc-gipzik dkuDum&quot;]/div[@class=&quot;m7p8f0-0 fUTidt&quot;]/div[@class=&quot;e9phw6-1-sc-kAzzGY JbQyp e9phw6-0 gUkenf&quot;]/div[1]/div[@class=&quot;eslncg-2 bKwwBK&quot;]/div[@class=&quot;eslncg-3 bScUdW&quot;]/div[@class=&quot;eslncg-4 gugdEn&quot;]/form[@class=&quot;lzru6b-0 dcOwwC&quot;]/div[2]/span[@class=&quot;dot6ul-4 kFSzbP&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
